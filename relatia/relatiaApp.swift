f7683d6fa537d021da014df600e0f2937b686b1a
gabinhallosserie <gabinhalloss@gmail.com>
2025-10-04 19:38:18 +0200

    Initial Commit

---
+++ relatia/relatiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  relatiaApp.swift
+//  relatia
+//
+//  Created by Gabin Hallosserie on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct relatiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
